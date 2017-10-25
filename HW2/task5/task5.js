11170a75c9c714a6476e66e1ea3dc7bd22ee80d5
Unknown <mr.kachanov@gmail.com>
2017-10-25 21:45:09 +0300

    end task5

--- HW2/task5/task5.js
+++ HW2/task5/task5.js
@@ -1,21 +1,52 @@
 var form = jQuery("form");
 var numbers = jQuery("#numbers");
 var letters = jQuery("#letters");
+var submit = jQuery("#submit");
+var checkbox = jQuery('input[type="checkbox"]');
+
+
+form.submit(function (event) {
+    checkInputs();
+    verifyCheckbox();
+    checkRadiobutton();
+});
+
+function verifyCheckbox(){
+    if(checkbox.prop("checked") === true){
+        return;
+    }else{
+        alert("Checkbox must be selected!");
+        event.preventDefault();
+    }
+}
+
+function checkRadiobutton() {
+    if ( jQuery('input[name=type]:checked').val() ) {
+        return;
+    } else {
+        alert("Radiobutton must be selected");
+        event.preventDefault();
+    }
+}
+
+function checkInputs() {
+    if( (numbers.prop("value") !== "") && (letters.prop("value")!== "") ){
+        return;
+    } else {
+        alert("Error");
+        event.preventDefault();
+    }
+}
 
 
 numbers.keypress(function (event) {
     if(event.charCode < 48 || event.charCode > 57){
-        return false;
+        event.preventDefault();
     }
-    console.log(numbers.prop("value"));
-})
+});
 
 letters.keypress(function (event) {
     if(event.charCode < 97 || event.charCode > 100){
-        return false;
+        event.preventDefault();
     }
-})
-
-form.on("submit", function () {
-
-})
+});
