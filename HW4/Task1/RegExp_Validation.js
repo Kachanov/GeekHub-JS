8180ffdd8a95b018526d5391f3d05359471e7bcb
Unknown <mr.kachanov@gmail.com>
2017-11-09 00:43:32 +0200

    Add validation with regular expressions

---
+++ HW4/Task1/RegExp_Validation.js
@@ -0,0 +1,64 @@
+var form = jQuery("form");
+var numbers = jQuery("#numbers");
+var letters = jQuery("#letters");
+var submit = jQuery("#submit");
+var checkbox = jQuery('input[type="checkbox"]');
+
+
+form.submit(function (event) {
+    checkEmptyInputs();
+    verifyCheckbox();
+    checkRadiobutton();
+});
+
+
+function checkEmptyInputs() {
+    if((numbers.prop("value") === "") && (letters.prop("value") === "")){
+        alert("Please enter information");
+        event.preventDefault();
+    }else {
+        checkNumbers();
+        checkLetters();
+    }
+}
+
+function checkNumbers() {
+    var rule = /^\d+$/;
+
+    if(!numbers.prop("value").match(rule)){
+        alert("Numbers must contain only numbers");
+        event.preventDefault();
+    }
+}
+
+function checkLetters() {
+    var rule = /^[a-dA-D]+$/;
+
+    if(!letters.prop("value").match(rule)){
+        alert("Letters must contain only symbols from A to D");
+        event.preventDefault();
+    }
+}
+
+
+function verifyCheckbox(){
+    if(checkbox.prop("checked") === true){
+        return;
+    }else{
+        alert("Checkbox must be selected");
+        event.preventDefault();
+    }
+}
+
+function checkRadiobutton() {
+    if (jQuery('input[name=type]:checked').val()) {
+        return;
+    } else {
+        alert("Radiobutton must be selected");
+        event.preventDefault();
+    }
+}
+
+
+
+
