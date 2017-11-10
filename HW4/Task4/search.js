285dbc5ec4a13e973f5030a2e5cace9460714a15
Unknown <mr.kachanov@gmail.com>
2017-11-10 16:53:36 +0200

    fix task4

--- HW4/Task4/search.js
+++ HW4/Task4/search.js
@@ -4,14 +4,19 @@
 
     searchInput.on("keyup", function () {
         var searchValue = searchInput.val();
+        var regularSearch = new RegExp(escapeRegExp(searchValue));
 
         elements.each(function (index) {
-            if (!jQuery(elements[index]).text().match(searchValue)) {
-                jQuery(elements[index]).fadeOut(100);
-            } else {
+            if (jQuery(elements[index]).text().match(regularSearch)) {
                 jQuery(elements[index]).fadeIn(100);
+            } else {
+                jQuery(elements[index]).fadeOut(100);
             }
         })
     });
+
+    function escapeRegExp(text){
+        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    }
 
 })()
