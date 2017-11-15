fa3f767bdb617222412ea30311e46c7d4bdd8f5f
Unknown <mr.kachanov@gmail.com>
2017-11-15 15:36:21 +0200

    task1.1

---
+++ HW5/Task1/task1_1.js
@@ -0,0 +1,24 @@
+Array.prototype.filterWhereRegExp = function (dataArray) {
+  var newArray = [];
+
+  for(var i = 0; i < this.length; i++){
+      for(var key in dataArray){
+          //if(this[i][key].match(dataArray[key])){
+          if(dataArray[key].test(this[i][key])){
+              newArray.push(this[i]);
+          }
+      }
+  }
+
+  return newArray;
+};
+
+
+var users = [{id: 1, name: "Max", age: 18},
+             {id: 2, name: "Bob", age: 22},
+             {id: 3, name: "Nick", age: 18}];
+
+
+var filterRegExp = users.filterWhereRegExp({name: /^max$/i});
+
+console.log(filterRegExp);
