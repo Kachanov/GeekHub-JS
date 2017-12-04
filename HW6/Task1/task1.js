005a593d107c62e8179faa12418e216b892d7589
Unknown <mr.kachanov@gmail.com>
2017-12-05 00:38:41 +0200

    fix dragging hw6

--- HW6/Task1/task1.js
+++ HW6/Task1/task1.js
@@ -4,7 +4,7 @@
     var total = 0;
     var itemNumber = 0;
     var itemQuantity = 1;
-
+    var cart;
     jQuery("#products > * [data-drag-helper]").on("mousedown", function (event) {
         var helper = jQuery(event.currentTarget);
         var clone = helper.clone();
@@ -43,16 +43,16 @@
 
 
 
-
+        var cloneRect;
         body.on("mousemove.dragProduct", function (event) {
             clone.css({
                 top: event.pageY - offset.top,
                 left: event.pageX - offset.left
             });
 
-            var cloneRect = clone.get(0).getBoundingClientRect();
+            cloneRect = clone.get(0).getBoundingClientRect();
 
-            var cart = carts.find(function (cart) {
+            cart = carts.find(function (cart) {
                 return overlaps(cart.rect, cloneRect);
             });
 
@@ -84,8 +84,11 @@
             newElement.find(".table-column-price").text(itemPrice);
             //newElement.find(".table-column-quantity").text(itemQuantity);
 
+            if(cart){
+                jQuery("[data-cart]").append(newElement);
+            }
 
-            jQuery("[data-cart]").append(newElement);
+            //jQuery("[data-cart]").append(newElement);
             jQuery("[data-cart]").removeClass("overlaps");
 
             helper.closest("[data-product]").removeClass("dragging");
